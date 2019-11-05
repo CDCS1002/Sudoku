68c661f17c811e92d15cf03b9f4bef354af6e43c
CDCS1002 <carlos.daniel.calderon.cdcs@gmail.com>
2019-11-05 11:56:45 -0600

    Se creo la interfaz grafica

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,98 @@
+#--------------------Imports del programa--------------------
+"""
+Aqui se realizan todos los imports que vaya a utilizar dentro del programa
+Entradas: Los modulos a los que llamo
+Salidas: Las funciones para los cuales los llame
+Restricciones: Estan basados en la version python 3.7
+"""
 from tkinter import *
+from tkinter import messagebox
+
+
+
+
+
+
+
+#--------------------Funciones del Menu desplegable (Basicas)--------------------
+#---Funcion de Ayuda---
+def ayudaMenu():
+	"""
+	Esta funcion es la que muestra el manual de usuario
+	Entradas: No recibe
+	Salidas: El manual de usuario
+	Restricciones: No tiene
+	"""
+	#wb.open_new(r"")
+
+
+#---Funcion de Salir---
+def salirMenu():
+	"""
+	Esta pequena funcion basica lo unico que realiza es que a la hora
+	de que se quiera salir del juego, este lo termina eliminando la 
+	raiz con todo aquello que tenga dentro
+	Entradas: No recibe
+	Salidas: La raiz quitada
+	Restricciones: No tiene
+	"""
+	root.quit()
+
+#---Funcion Acerca De---
+def acercaDeMenu():
+	"""
+	El acerca de es una parte del menu de la parte de arriba en la cual si el usuario
+	presiona va a desplegar un pequeno messagebox de informacion sobre todo el programa
+	para que el usuario pueda saber el nombre del programa, la version, la fecha de creacion 
+	y el autor.
+	Entradas: No recibe
+	Salidas: El messagebox con la informacion correspondiente
+	Restricciones: No tiene 
+	"""
+	messagebox.showinfo("INFORMACION DEL PROGRAMA", "NOMBRE: SUDOKU\nAUTOR:Carlos Calderon\nFECHA DE CREACION: 5/11/2019 \nVERSION:1.0.0")
+
+#--------------------Interfaz Grafica Principal (root)--------------------
+"""
+Esta es la forma en la que creo mi ventana principal y la 
+llamo de la forma root para futuras menciones, tiene un tamano
+de 650x650 y es de color blanca
+Entradas: No recibe
+Salidas: Crea la ventana principal con el menu de arriba para que de ahi se elija que hacer
+Restricciones: No recibe
+"""
+root=Tk()
+root.title("SUDOKU")
+root.iconbitmap("icono_sudoku.ico")
+root.geometry("650x650")
+root.config(bg="white")
+
+#----------Menu desplegable tipo IDLE---------
+"""
+Todo esto son botones tipo menus que aparecen encima del juego
+con diferentes opciones para que el usario pueda interactuar
+Entradas: No recibe
+Salidas: Esto crea el menu y lo coloca en la parte arriba del root
+Restricciones: No tiene
+"""
+vinetas= Menu()
+root.config(menu=vinetas)
+
+BarraJugar=Menu(vinetas, tearoff=False)
+vinetas.add_command(label="Jugar")
+
+
+BarraConfigurar=Menu(vinetas, tearoff=False)
+vinetas.add_command(label="Configurar")
+
+BarraAyuda=Menu(vinetas, tearoff=False)
+vinetas.add_cascade(label="Ayuda", command=ayudaMenu)
+
+BarraAcerca=Menu(vinetas, tearoff=False)
+vinetas.add_command(label="Acerca de", command=acercaDeMenu)
+
+BarraSalir=Menu(vinetas, tearoff=False)
+vinetas.add_command(label="Salir", command=salirMenu)
+
+
+
+root.mainloop()
